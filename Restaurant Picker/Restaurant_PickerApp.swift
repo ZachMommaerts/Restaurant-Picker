b1df629a2b9b3846412133bd797f2bde0c6f2bc3
ZachMommaerts <zmommaerts@gmail.com>
2023-04-11 16:15:21 -0500

    Initial Commit

---
+++ Restaurant Picker/Restaurant_PickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Restaurant_PickerApp.swift
+//  Restaurant Picker
+//
+//  Created by Zach Mommaerts on 4/11/23.
+//
+
+import SwiftUI
+
+@main
+struct Restaurant_PickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
